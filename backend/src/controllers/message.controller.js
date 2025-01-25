3631b5f72a325ca1a107d8a94f40004e3c0c60a1
ramimarogy <ramimarogy@yahoo.com>
2025-01-24 21:22:25 -0500

    completed function for getMessages (get messages between two users)

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,6 @@
+import User from "../models/user.model.js"
+import Message from "../models/message.model.js"
+
 export const getUsersForSidebar = async (req, res) => {
   try {
     const loggedInUserId = req.user._id
@@ -6,6 +9,25 @@ export const getUsersForSidebar = async (req, res) => {
     res.status(200).json(filteredUsers)
   } catch (error) {
     console.error("Error in getUsersForSidebar: ", error.message)
+    res.status(500).json({ error: "Internal server error" })
+  }
+}
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params
+    const myId = req.user._id
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ]
+    })
+
+    res.status(200).json(messages)
+  } catch (error) {
+    console.log("Error in getMessages controller: ", error.message)
     res.status(500).json({ error: "Internal server error" })
   }
 }
