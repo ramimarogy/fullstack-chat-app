ca82df6e383063b4560a25b7cd5786b1e6246adb
ramimarogy <ramimarogy@yahoo.com>
2025-02-02 10:05:50 -0500

    added and initialized socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,9 @@ import {connectDB} from "./lib/db.js"
 
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
+import { app, server } from "./lib/socket.js"
 
 dotenv.config()
-const app = express()
 
 const PORT = process.env.PORT
 
@@ -17,15 +17,16 @@ app.use(express.json({
     limit: "50mb"
 }))
 app.use(cookieParser())
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true
 }))
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("Server is running on PORT:" + PORT)
     connectDB()
 })
